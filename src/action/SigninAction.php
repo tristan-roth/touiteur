d12dfb4f6ef0fd18f90d92117231ed5ea8455173
Eylexander <eylexander88@gmail.com>
2023-11-06 16:04:03 +0100

    Repair

--- src/action/SigninAction.php
+++ src/action/SigninAction.php
@@ -1,25 +1,31 @@
 <?php
+declare(strict_types=1);
+
 namespace iutnc\touiteur\action;
-require_once "vendor/autoload.php";
-use iutnc\touiteur\action\Action as Action;
+
+use iutnc\touiteur\action\Action;
 use iutnc\touiteur\connect\ConnectionFactory;
-class SigninAction extends Action{
+
+class SigninAction extends Action {
+
     public function __construct(){
-        parent();
+        parent::__construct();
     }
 
-    public function execute() : boolean{
+    public function execute() : string {
         $method = $_SERVER["REQUEST_METHOD"];
         $html="";
+
         if ($method === "GET"){
-            $html = $html . "<h3>connexion : </h3>
+            $html = <<<HTML
+            <h3>connexion : </h3>
             <form action id = signin method = \"POST\">
-            <input type = \"text\" id = \"mail\" name = \"mail\" placeholder = \"votre mail\">
-            <input type = \"text\" id = \"mdp\" name = \"mdp\" placeholder = \"votre mot de passe\">
-            <button type = \"submit\">valider</button>
-            </form>";
-        }
-        else{
+                <input type = "text" id = "mail" name = "mail" placeholder = "votre mail">
+                <input type = "text" id = "mdp" name = "mdp" placeholder = "votre mot de passe">
+                <button type = "submit">Valider</button>
+            </form>
+            HTML;
+        } else if ($method === "POST") {
             ConnectionFactory::setConfig("config.ini");
             $mdp = password_hash($_POST["mdp"],1);
             $email = filter_var($_POST["mail"],FILTER_SANITIZE_EMAIL);
