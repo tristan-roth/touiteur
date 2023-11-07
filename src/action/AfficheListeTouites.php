5bf3d9eb90fecef73382fa3865a1823e28efb31e
LaheurteR <97089676+LaheurteR@users.noreply.github.com>
2023-11-07 17:02:59 +0100

    regle les images

--- src/action/AfficheListeTouites.php
+++ src/action/AfficheListeTouites.php
@@ -9,23 +9,24 @@ class AfficheListeTouites extends Action {
     {
         ConnectionFactory::setConfig("config.ini");
         $connexion = ConnectionFactory::makeConnection();
-        $data = $connexion->query("select message_text, images.image_path as image from touits inner join images on touits.id_image = images.id_image
+        $data = $connexion->query("select message_text, images.image_path as image from touits left join images on touits.id_image = images.id_image
                                      order by touits.id_touit desc");
         $html = "";
         while ($res=$data->fetch()){
             $message = htmlspecialchars($res['message_text']);
             $html .= "<p>$message</p>";
-            if ($res['image'] !== "null"){
+            var_dump($res["image"]);
+            if ($res['image'] !== null){
                 $element = explode(".",$res['image']);
                 switch($element[count($element)-1]){
                     case "mp4" :
                         $html.='<video controls width="250">
                         <source src="upload/'.$res['image'].'" type="video/mp4" />
-                        <a href=""upload/'.$res['image'].'"></a>
+                        <a href="upload/'.$res['image'].'"></a>
                         </video>';
                         break;
                     default :
-                        $html .= "<img src='upload/".$res['image']."' width='300px' ><br>";
+                        $html .= '<img src="upload/'.$res['image'].'" width="300px" ><br>';
                 }
             
             
