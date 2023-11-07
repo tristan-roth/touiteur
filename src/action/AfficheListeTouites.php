00cc48d7f9060b6fc9a781837650cddc0c3f8f6d
Tristan ROTH <tristan.roth3@etu.univ-lorraine.fr>
2023-11-07 17:24:55 +0100

    tweet détail OK

--- src/action/AfficheListeTouites.php
+++ src/action/AfficheListeTouites.php
@@ -9,14 +9,13 @@ class AfficheListeTouites extends Action {
     {
         ConnectionFactory::setConfig("config.ini");
         $connexion = ConnectionFactory::makeConnection();
-        $data = $connexion->query("select message_text, images.image_path as image from touits left join images on touits.id_image = images.id_image
+        $data = $connexion->query("select id_touit, message_text, images.image_path as image from touits left join images on touits.id_image = images.id_image
                                      order by touits.id_touit desc");
         $html ='';
-        $id = 0;
         while ($res=$data->fetch()){
             $message = htmlspecialchars($res['message_text']);
+            $id = $res['id_touit'];
             $html .= '<a href="?action=detail&id='.$id.'">';
-            $id++;
             $html .= "<p>$message</p>";
             if ($res['image'] !== null){
                 $element = explode(".",$res['image']);
--- src/action/AfficheTouite.php
+++ src/action/AfficheTouite.php
@@ -8,27 +8,26 @@ class AfficheTouite extends Action{
 
     public function execute(): string
     {
+        $id = $_GET['id'];
         ConnectionFactory::setConfig("config.ini");
         $connexion = ConnectionFactory::makeConnection();
-        $data = $connexion->query("select message_text, date_touit, rating, images.image_path as image, touitsutilisateur.id_utilisateur as id_user from touits 
-                                    inner join images on touits.id_image = images.id_image
+        $data = $connexion->query("select message_text, date_touit, rating, images.image_path as image, utilisateur.utilisateur as id_user from touits 
+                                    left join images on touits.id_image = images.id_image
                                     inner join touitsutilisateur on touits.id_touit = touitsutilisateur.id_touit
+                                    inner join utilisateur on touitsutilisateur.id_utilisateur = utilisateur.id_utilisateur
+                                    where touits.id_touit = $id
                                     order by touits.id_touit desc");
         $html = "";
-        var_dump($data);
-        var_dump($data->fetch());
         while ($res=$data->fetch()){
-            echo 'coucou';
-            var_dump($res['id_user']);
             $message = htmlspecialchars($res['message_text']);
             $html .= "<p>$message</p>";
-            if ($res['image'] !== "null"){
+            if ($res['image'] !== null){
                 $element = explode(".",$res['image']);
                 switch($element[count($element)-1]){
                     case "mp4" :
                         $html.='<video controls width="250">
                         <source src="upload/'.$res['image'].'" type="video/mp4" />
-                        <a href=""upload/'.$res['image'].'"></a>
+                        <a href="upload/'.$res['image'].'"></a>
                         </video>';
                         break;
                     default :
@@ -36,7 +35,7 @@ class AfficheTouite extends Action{
                 }
             }
             $html .= "<p>date : ".$res['date_touit']."</p>";
-            $html .= "<p>rating : ".$res['rating_touit']."</p>";
+            $html .= "<p>rating : ".$res['rating']."</p>";
             $html .= "<p>Auteur : ".$res['id_user']."</p>";
             $html .= <<<HTML
                 <form action="index.php" method="post">
