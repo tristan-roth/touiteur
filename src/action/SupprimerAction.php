71e36c04a7f9cff3bef7540fb53b837e99ce0bd6
Tristan ROTH <tristan.roth3@etu.univ-lorraine.fr>
2023-11-09 13:07:49 +0100

    suppression presque fini

--- src/action/SupprimerAction.php
+++ src/action/SupprimerAction.php
@@ -11,12 +11,25 @@ class SupprimerAction extends Action{
         ConnectionFactory::setConfig("config.ini");
         $connexion = ConnectionFactory::makeConnection();
         $id = $_GET['id'];
-        $data = $connexion->query("select utilisateur from utilisateur 
+        $data = $connexion->query("select utilisateur, images.id_image as image from utilisateur 
                                    inner join touitsutilisateur on utilisateur.id_utilisateur = touitsutilisateur.id_utilisateur
+                                   inner join touits on touits.id_touit = touitsutilisateur.id_touit
+                                   left join images on images.id_image = touits.id_image
                                    where touitsutilisateur.id_touit = $id");
         $res = $data->fetch();
+        var_dump($res);
+        $image = $res['image'];
         if ($res['utilisateur'] === $_SESSION['login']){
-            $data = $connexion->query("delete from touits where id_touit = $id");
+            if ($image !== null){
+                $data = $connexion->prepare("delete from images where id_image = ?");
+                $data->execute([$image]);
+            }
+            $data = $connexion->prepare("delete from touits where id_touit = ?");
+            $data->execute([$id]);
+            $data = $connexion->prepare("delete from touitsutilisateur where id_touit = ?");
+            $data->execute([$id]);
+
+
             $contenuHtml = "<h2>Le touite a bien été supprimé</h2>";
         } else {
             $contenuHtml = "<h2>Vous ne pouvez pas supprimer ce touite</h2>";
